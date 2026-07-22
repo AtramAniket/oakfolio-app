28014896dd8743006bef3b7cb6765f46fb5ac70b
AtramAniket <aniket_atram@live.in>
2026-07-22 14:43:39 +0530

    Added basic Ui for registering new user

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -11,15 +11,24 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 
+import { useNavigate } from 'react-router'
+
 
 const RegisterPage = () => {
+
+	const navigateTo = useNavigate()
+
+	const handleLoginPageRedirect = () => {
+		navigateTo('/login')
+	}
+	
 	return (
 		<>
 			<div className='flex min-h-screen items-center justify-center px-4'>
 				<Card className='w-full max-w-md'>
 				  <CardHeader className="space-y-2">
 				    <CardTitle>Create new account</CardTitle>
-				    <CardDescription>Enter email address for sending verification link</CardDescription>
+				    <CardDescription>Enter your email address to receive a verification link</CardDescription>
 				  </CardHeader>
 				  <CardContent>
 				    <form className="space-y-6">
@@ -32,11 +41,16 @@ const RegisterPage = () => {
 				  					placeholder='johndoe@example.com'
 				    			/>
 				    		</div>
+
+				    		<Button className="w-full" type='submit'>Register</Button>
 				    </form>
 				  </CardContent>
-				  <CardFooter className="justify-center">
-				    <Button className="w-full" type='submit'>Register</Button>
-				  </CardFooter>
+				  <CardFooter className="justify-center text-sm text-muted-foreground">
+          	Already have an account?
+          	<Button variant="link" type="button" onClick={handleLoginPageRedirect} className="px-1">
+            	Login
+          	</Button>
+        	</CardFooter>
 				</Card>
 			</div>
 		</>
