1d29cd1e68852ac99aa9861021429f7abc7290a5
AtramAniket <aniket_atram@live.in>
2026-07-22 15:00:39 +0530

    Added basic ui for setting password

--- src/pages/auth/SetPasswordPage.jsx
+++ src/pages/auth/SetPasswordPage.jsx
@@ -1,7 +1,55 @@
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+
+
 const SetPasswordPage = () => {
+
 	return (
 		<>
-			<h1>Set Password Page</h1>
+			<div className='flex min-h-screen items-center justify-center px-4'>
+				<Card className='w-full max-w-md'>
+				  <CardHeader className="space-y-2">
+				    <CardTitle>Set your password</CardTitle>
+				    <CardDescription>Choose a strong password to secure your Oakfolio account.</CardDescription>
+				  </CardHeader>
+				  <CardContent>
+				    <form className="space-y-6">
+				    		
+				    		<div className="space-y-2">
+				    			<Label htmlFor='password'>Password</Label>
+				    			<Input
+				    				required
+				    				id='password'
+				    				type='password'
+				  					placeholder='Enter your password'
+				    			/>
+				    		</div>
+				    		
+				    		<div className="space-y-2">
+				    			<Label htmlFor='confirm-password'>Confirm Password</Label>
+				    			<Input
+				    				required
+				    				type='password'
+				    				id='confirm-password'
+				    				placeholder='Confirm your password'
+				    			/>
+				    		</div>
+
+            		<Button className="w-full" type='submit'>
+            			Set Password
+            		</Button>
+				    </form>
+				  </CardContent>
+				</Card>
+			</div>
 		</>
 		)
 }
