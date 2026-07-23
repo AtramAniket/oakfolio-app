94c0d07f47dec25fb68b7a40b282f7ce65a1402e
AtramAniket <aniket_atram@live.in>
2026-07-23 12:40:34 +0530

    Moved useAuth to a separate file

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,14 +1,3 @@
 import { createContext, useContext } from 'react'
 
-export const AuthContext = createContext(null);
-
-export const useAuth = () => {
-	
-	const context = useContext(AuthContext)
-	
-	if(!context){
-		throw new Error('Auth must be used withing AuthProvider')
-	}
-
-	return context
-}
+export const AuthContext = createContext(null)
