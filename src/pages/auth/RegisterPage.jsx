793a4eba06b40e3c4b184df7ecee2a80099fec8e
AtramAniket <aniket_atram@live.in>
2026-07-23 14:31:17 +0530

    Added API call to submit method

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -11,12 +11,52 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 
+import { useState } from 'react'
 import { useNavigate } from 'react-router'
+import authService  from '@/services/authService'
 
 
 const RegisterPage = () => {
 
 	const navigateTo = useNavigate()
+
+	const [error, setError] = useState('')
+	const [email, setEmail] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	const handleFormSubmit = async (event) => {
+		event.preventDefault()
+		
+		setLoading(true)
+
+		try{
+
+			const response = await authService.register({
+				email,
+			})
+
+			navigateTo('/check-email', {
+				state: {
+					email,
+					response,
+				},
+			})
+		}
+		catch(err){
+			setError(
+				err.response?.data?.detail ??
+				'Something went wrong. Please try again' 
+			)
+		}
+		finally{
+			setLoading(false)
+		}
+	}
+
+	const handleEmailInputChange = (event) => {
+		const { value } = event.target
+		setEmail(value)
+	}
 
 	const handleLoginPageRedirect = () => {
 		navigateTo('/login')
@@ -38,16 +78,34 @@ const RegisterPage = () => {
 				    				required
 				    				id='email'
 				    				type='email'
+				    				value={email}
 				  					placeholder='johndoe@example.com'
+				  					onChange={(event) => handleEmailInputChange(event)}
 				    			/>
 				    		</div>
 
-				    		<Button className="w-full" type='submit'>Register</Button>
+				    		{
+				    			error && (
+				    				<p className='text-sm text-destructive'>
+				    					{error}
+				    				</p>
+				    			)
+				    		}
+
+				    		<Button 
+				    			type='submit'
+				    			disabled={loading}
+				    			className="w-full"
+				    			onClick={(event) => handleFormSubmit(event)}
+				    		>
+				    			{loading ? 'Creating account...' : 'Create Account'}
+				    		</Button>
 				    </form>
 				  </CardContent>
 				  <CardFooter className="justify-center text-sm text-muted-foreground">
           	Already have an account?
-          	<Button variant="link" type="button" onClick={handleLoginPageRedirect} className="px-1">
+          	<Button 
+          		variant="link" type="button" onClick={handleLoginPageRedirect} className="px-1">
             	Login
           	</Button>
         	</CardFooter>
