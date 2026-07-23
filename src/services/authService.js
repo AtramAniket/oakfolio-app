f1a8ed55fb2163efb8c93bd5ec21a2fadb8a9e4d
AtramAniket <aniket_atram@live.in>
2026-07-23 16:14:31 +0530

    Added token verification to auth service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,13 @@
 import api from '@/api/api'
 
 const authService = {
-	register: async (data) =>{
+	register: async (data) => {
 		const response = await api.post('/auth/register', data)
 		return response?.data
+	},
+
+	verify_token: async (data) => {
+		const response = await api.post('/auth/verify-registration-token', data)
 	},
 
 	login: async (data) => {
