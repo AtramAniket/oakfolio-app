c3fa39d944479264bd846c897bd3e7e65d92f828
AtramAniket <aniket_atram@live.in>
2026-07-28 14:30:27 +0530

    Added common loader for pages

---
+++ src/components/common/Spinner.jsx
@@ -0,0 +1,11 @@
+import { Loader2 } from 'lucide-react'
+
+import React from 'react'
+
+const Spinner = () => {
+	return (
+			<Loader2 className="mx-auto h-12 w-12 animate-spin text-primary" />
+			)
+}
+
+export default Spinner
---
+++ src/pages/common/LoadingPage.jsx
@@ -0,0 +1,23 @@
+import Spinner from '@/components/common/Spinner'
+
+const LoadingPage = ({ 
+	message='Restoring your session...'
+}) => {
+	return (
+		<div flex min-h-screen flex-col items-center justify-center bg-background px-6>
+			<h2 className="mt-6 text-lg font-medium text-foreground">
+				{message}
+			</h2>
+
+			<p className="mt-2 text-sm text-muted-foreground">
+				Oakfolio
+			</p>
+
+			<p className="text-xs text-muted-foreground">
+				Secure Portfolio Tracking
+			</p>
+		</div>
+	)
+}
+
+export default LoadingPage
