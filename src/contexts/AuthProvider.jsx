1594ac5daaa71a91238f06f4b75b898f7bfec2b8
AtramAniket <aniket_atram@live.in>
2026-07-28 14:06:17 +0530

    Added login, logout and token refresh logic

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,23 +1,101 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 
-import { AuthContext } from './AuthContext'
+import authService from '@/services/authService'
+import { AuthContext } from '@/contexts/AuthContext'
 
 const AuthProvider = ({ children }) => {
 	
 	const [user, setUser] = useState(null)
 	const [loading, setLoading] = useState(false)
 
-	const isAuthenticated = !user
+	const isAuthenticated = !!user
+
+	const clearAuth = () => {
+		localStorage.removeItem('access_token')
+		setUser(null)
+	}
+
+	const refreshUser = async () => {
+		const token = localStorage.getItem('access_token')
+
+		if(!token){
+			clearAuth()
+			return null
+		}
+
+		setLoading(true)
+
+		try{
+			const currentUser = await authService.getCurrentUser()
+
+			setUser(currentUser)
+
+			return currentUser
+		}
+		catch(err){
+			clearAuth()
+			throw err
+		}
+		finally{
+			setLoading(false)
+		}	
+	}
+
+
+	const login = async (data) => {
+		setLoading(true)
+
+		try{
+			const response = await authService.login(data)
+
+			localStorage.setItem('access_token', response.access_token)
+
+			return await refreshUser()
+			
+		}
+		catch(err){
+			clearAuth()
+			throw err
+		}
+		finally{
+			setLoading(false)
+		}
+	}
+
+
+	const logout = () => {
+		clearAuth()
+	}
+
+
+	const hydrateUser = async () => {
+		try{
+			await refreshUser()
+		}
+		catch(err){
+			// later add something specific here
+		}
+	}
+
+
+
+	// Hydrate user details when component mounts
+	useEffect(() => {
+		hydrateUser()
+	}, [])
+
+
 
 	const value = useMemo(
 		() => ({
 			user,
-			setUser,
+			login,
+			logout,
 			loading,
-			setLoading,
+			refreshUser,
 			isAuthenticated,
 		}),
-		[user, loading]
+		[user, loading, refreshUser]
 	)
 
 	return(
