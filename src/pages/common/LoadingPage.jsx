5f88af6fd84d2977c789fbbb9c829cf40705f646
AtramAniket <aniket_atram@live.in>
2026-07-29 15:48:24 +0530

    Added common loading component

--- src/pages/common/LoadingPage.jsx
+++ src/pages/common/LoadingPage.jsx
@@ -4,16 +4,16 @@ const LoadingPage = ({
 	message='Restoring your session...'
 }) => {
 	return (
-		<div flex min-h-screen flex-col items-center justify-center bg-background px-6>
-			<h2 className="mt-6 text-lg font-medium text-foreground">
+		<div className='flex min-h-screen flex-col items-center justify-center bg-background px-6'>
+			<h2 className='mt-6 text-lg font-medium text-foreground'>
 				{message}
 			</h2>
 
-			<p className="mt-2 text-sm text-muted-foreground">
+			<p className='mt-2 text-sm text-muted-foreground'>
 				Oakfolio
 			</p>
 
-			<p className="text-xs text-muted-foreground">
+			<p className='text-xs text-muted-foreground'>
 				Secure Portfolio Tracking
 			</p>
 		</div>
