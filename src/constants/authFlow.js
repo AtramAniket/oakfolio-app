33efd07a40d5946f4b4a2df1f9ec9b24ba42aabf
AtramAniket <aniket_atram@live.in>
2026-07-29 15:44:41 +0530

    Added defaults for step forms

---
+++ src/constants/authFlow.js
@@ -0,0 +1,11 @@
+export const REGISTER_STEPS = {
+  FORM: 'form',
+  CHECK_EMAIL: 'check-email',
+}
+
+export const VERIFY_STEPS = {
+  LOADING: 'loading',
+  PASSWORD: 'password',
+  COMPLETED: 'completed',
+  FAILURE: 'failure',
+}
