675354d3bd5cf728f63db982fd5ac6e3f7f661a7
AtramAniket <aniket_atram@live.in>
2026-07-29 15:47:09 +0530

    Added service for new user registration

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,16 @@
 import api from '@/api/api'
 
 const authService = {
+	getCurrentUser: async () => {
+		const response = await api.get('/auth/me')
+		return response?.data
+	},
+
+	login: async (data) => {
+		const response = await api.post('/auth/login', data)
+		return response?.data
+	},
+
 	register: async (data) => {
 		const response = await api.post('/auth/register', data)
 		return response?.data
@@ -11,15 +21,10 @@ const authService = {
 		return response?.data
 	},
 
-	login: async (data) => {
-		const response = await api.post('/auth/login', data)
+	createNewUser: async (data) => {
+		const response = await api.post('/auth/create_user', data)
 		return response?.data
 	},
-
-	getCurrentUser: async () => {
-		const response = await api.get('/auth/me')
-		return response?.data
-	}
 }
 
 export default authService
