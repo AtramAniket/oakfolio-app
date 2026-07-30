c9739fe0d43cbfe35e5c5b79178faf3de72feb5e
AtramAniket <aniket_atram@live.in>
2026-07-30 12:58:20 +0530

    Added dashbaord layout

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom'
+
+import AppTopbar from '@/components/layout/AppTopbar'
+import AppSidebar from '@/components/layout/AppSidebar'
+
+const DashboardLayout = () => {
+	return (
+		<div className='flex min-h-screen'>
+			<AppSidebar />
+
+			<div className='flex flex-1 flex-col'>
+				<AppTopbar />
+
+				<main className='flex-1'>
+					<Outlet />
+				</main>
+			</div>
+		</div>
+)}
+
+export default DashboardLayout
