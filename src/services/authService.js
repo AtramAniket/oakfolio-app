7a3247c8a31d6bd608e757a75d1d3a28e70abcd1
AtramAniket <aniket_atram@live.in>
2026-08-14 15:21:00 +0530

    Fixed api routes

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,27 +2,27 @@ import api from '@/api/api'
 
 const authService = {
 	getCurrentUser: async () => {
-		const response = await api.get('/auth/me')
+		const response = await api.get('/api/v1/auth/me')
 		return response?.data
 	},
 
 	login: async (data) => {
-		const response = await api.post('/auth/login', data)
+		const response = await api.post('/api/v1/auth/login', data)
 		return response?.data
 	},
 
 	register: async (data) => {
-		const response = await api.post('/auth/register', data)
+		const response = await api.post('/api/v1/auth/register', data)
 		return response?.data
 	},
 
 	verifyToken: async (data) => {
-		const response = await api.post('/auth/verify-registration-token', data)
+		const response = await api.post('/api/v1/auth/verify-registration-token', data)
 		return response?.data
 	},
 
 	createNewUser: async (data) => {
-		const response = await api.post('/auth/create_user', data)
+		const response = await api.post('/api/v1/auth/create_user', data)
 		return response?.data
 	},
 }
--- src/services/portfolioService.js
+++ src/services/portfolioService.js
@@ -2,42 +2,42 @@ import api from '@/api/api'
 
 const portfolioService = {
 	getPortfolios: async () => {
-		const response = await api.get('/stocks/portfolios')
+		const response = await api.get('/api/v1/stocks/portfolios')
 		return response?.data
 	},
 
     createNewPortfolio: async (data) => {
-        const response = await api.post('/stocks/portfolios', data)
+        const response = await api.post('/api/v1/stocks/portfolios', data)
         return response?.data
     },
 
     deletePortfolio: async (portfolio_id) => {
-        const response = await api.delete(`/stocks/portfolios/${portfolio_id}`)
+        const response = await api.delete(`/api/v1/stocks/portfolios/${portfolio_id}`)
         return response?.data
     },
 
     editPortfolio: async (portfolio_id, data) => { 
-        const response = await api.put(`/stocks/portfolios/${portfolio_id}`, data)
+        const response = await api.put(`/api/v1/stocks/portfolios/${portfolio_id}`, data)
         return response?.data
     },
 
     getAllHoldings: async (portfolio_id) => {
-        const response = await api.get(`/stocks/portfolios/${portfolio_id}/holdings`)
+        const response = await api.get(`/api/v1/stocks/portfolios/${portfolio_id}/holdings`)
         return response?.data
     },
 
     addStockToPortfolio: async (portfolio_id, data) => {
-        const response = await api.post(`/stocks/portfolios/${portfolio_id}/holdings`, data)
+        const response = await api.post(`/api/v1/stocks/portfolios/${portfolio_id}/holdings`, data)
         return response?.data
     },
 
     deleteStockFromPortfolio: async (portfolio_id, holding_id) => {
-        const response = await api.delete(`/stocks/portfolios/${portfolio_id}/holdings/${holding_id}`)
+        const response = await api.delete(`/api/v1/stocks/portfolios/${portfolio_id}/holdings/${holding_id}`)
         return response?.data
     },
 
     editStockInPortfolio: async (portfolio_id, holding_id, data) => {
-        const response = await api.put(`/stocks/portfolios/${portfolio_id}/holdings/${holding_id}`, data)
+        const response = await api.put(`/api/v1/stocks/portfolios/${portfolio_id}/holdings/${holding_id}`, data)
         return response?.data
     },
 }
